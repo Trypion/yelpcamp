88085e1f4492577ff9e2e13d162801f4709e4b05
Israel <israel.schmitt.j@gmail.com>
2020-04-05 17:34:36 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,94 @@
+const express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require('mongoose'),
+  seedDB = require("./seeds"),
+  Campground = require("./models/campground");
+
+
+
+mongoose.connect('mongodb://localhost:27017/yelp_camp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.set("view engine", "ejs");
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+
+  console.log('conectado ao banco');
+  //console.log(Campground);
+
+  //preencher o banco
+  //seedDB();
+
+  app.get("/", function (req, res) {
+    res.render("landing");
+  });
+
+  app.get("/campgrounds", function (req, res) {
+
+    Campground.find({}, function (err, Allcampgrounds) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("campgrounds/index", {
+          campgrounds: Allcampgrounds
+        });
+      }
+    });
+  });
+
+  app.post("/campgrounds", function (req, res) {
+
+    const name = req.body.name;
+    const image = req.body.image;
+    const description = req.body.description;
+
+    const newCampground = {
+      name: name,
+      image: image,
+      description: description
+    };
+
+    Campground.create(newCampground, function (err, newlyCreated) {
+      if (err) {
+        console.log(err)
+      } else {
+        res.redirect("campgrounds/index");
+      }
+    });
+  });
+
+  app.get("/campgrounds/new", function (req, res) {
+    res.render("campgrounds/new");
+  });
+
+  app.get("/campgrounds/:id", function (req, res) {
+
+    //encontrar o campground com o ID
+    Campground.findById(req.params.id, function (err, foundCampground) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("campgrounds/show", {
+          campground: foundCampground
+        });
+      }
+
+    });
+
+  });
+
+
+  app.listen(3000, function () {
+    console.log("YelpCamp server Online, porta 3000");
+  });
+
+});
