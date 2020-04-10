a13327910e6ebebf1545f155af93747cfba15255
Israel <israel.schmitt.j@gmail.com>
2020-04-10 20:16:03 -0300

    comments pre style

--- app.js
+++ app.js
@@ -99,6 +99,43 @@ db.once('open', function () {
   });
 
 
+  //=============
+  //COMMENTS
+  //=============
+
+  app.get("/campgrounds/:id/comments/new", function (req, res) {
+    Campground.findById(req.params.id, function (err, campground) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("comments/new", {
+          campground: campground
+        });
+      }
+    });
+  });
+
+  app.post("/campgrounds/:id/comments", function (req, res) {
+
+    Campground.findById(req.params.id, function (err, campground) {
+      if (err) {
+        console.log(err);
+        res.redirect("/campgrounds");
+      } else {
+        Comment.create(req.body.comment, function (err, comment) {
+          if (err) {
+            console.log(err);
+          } else {
+            campground.comments.push(comment);
+            campground.save();
+            res.redirect("/campgrounds/" + campground._id);
+          }
+        })
+      }
+    })
+
+  });
+
   app.listen(3000, function () {
     console.log("YelpCamp server Online, porta 3000");
   });
