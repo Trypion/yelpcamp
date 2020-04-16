8c3a13714537f4ca3a236856acecab24c4a6dcb8
Israel <israel.schmitt.j@gmail.com>
2020-04-16 14:58:13 -0300

    comments com update e destroy

--- routes/comments.js
+++ routes/comments.js
@@ -36,10 +36,44 @@ router.post("/", isLoggedIn, function (req, res) {
                     campground.save();
                     res.redirect("/campgrounds/" + campground._id);
                 }
-            })
+            });
         }
-    })
+    });
+});
 
+//comments edit route
+router.get("/:comment_id/edit", checkCommentOwnership, (req, res) => {
+    Comment.findById(req.params.comment_id, (err, foundComment) =>{
+        if (err) {
+            console.log(err);
+            res.redirect("back");
+        } else {
+            res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+        }
+    });    
+});
+
+//comment update
+router.put("/:comment_id", checkCommentOwnership, (req, res) =>{   
+   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) =>{
+       if (err) {
+           console.log(err);
+           res.redirect("back");
+       } else {
+           res.redirect("/campgrounds/" + req.params.id);
+       }
+   });
+});
+
+//comment destroy route
+router.delete("/:comment_id", checkCommentOwnership, (req, res) =>{    
+    Comment.findByIdAndRemove(req.params.comment_id, (err) =>{
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
 });
 
 //middleware
@@ -48,6 +82,25 @@ function isLoggedIn(req, res, next) {
         return next();
     }
     res.redirect("/login");
+}
+
+function checkCommentOwnership (req, res, next) {
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, (err, foundComment) => {
+            if (err) {
+                console.log("err");
+                res.redirect("back");
+            } else {
+                if (foundComment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
 }
 
 module.exports = router;
